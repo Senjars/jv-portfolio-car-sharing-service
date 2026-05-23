e9c020deae5c9779464e592d63d202ad037e1dd7
Senjars <jaroslawsendecki1@gmail.com>
2026-05-23 21:35:31 +0200

    feat: enable async support for telegram notifications

--- src/main/java/com/github/senjars/carsharing/JvPortfolioCarSharingServiceApplication.java
+++ src/main/java/com/github/senjars/carsharing/JvPortfolioCarSharingServiceApplication.java
@@ -3,10 +3,12 @@ package com.github.senjars.carsharing;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class JvPortfolioCarSharingServiceApplication {
 
     public static void main(String[] args) {
@@ -16,5 +18,4 @@ public class JvPortfolioCarSharingServiceApplication {
 
         SpringApplication.run(JvPortfolioCarSharingServiceApplication.class, args);
     }
-
 }
--- src/main/java/com/github/senjars/carsharing/notify/TelegramService.java
+++ src/main/java/com/github/senjars/carsharing/notify/TelegramService.java
@@ -3,6 +3,7 @@ package com.github.senjars.carsharing.notify;
 import com.github.senjars.carsharing.config.TelegramConfig;
 import com.github.senjars.carsharing.exception.TelegramNotificationException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -14,6 +15,7 @@ public class TelegramService {
     private final TelegramBot telegramBot;
     private final TelegramConfig telegramConfig;
 
+    @Async
     public void sendMessage(String message) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(telegramConfig.getChatId());
