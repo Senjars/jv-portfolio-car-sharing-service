6fce0e7a94eb53e82be1b85b87ac0c13d719b08a
Senjars <jaroslawsendecki1@gmail.com>
2026-06-28 21:53:32 +0200

    chore(db): update payment-rental schema constraints and add missing car types in liquibase

--- src/main/java/com/github/senjars/carsharing/mapper/PaymentMapper.java
+++ src/main/java/com/github/senjars/carsharing/mapper/PaymentMapper.java
@@ -3,10 +3,12 @@ package com.github.senjars.carsharing.mapper;
 import com.github.senjars.carsharing.dto.payment.PaymentResponseDto;
 import com.github.senjars.carsharing.model.payment.Payment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PaymentMapper {
 
+    @Mapping(target = "rentalId", expression = "java(String.valueOf(payment.getRental().getId()))")
     PaymentResponseDto toDto(Payment payment);
 
     Payment toEntity(PaymentResponseDto paymentResponseDto);
